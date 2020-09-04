8fa4654be31cda8234a2388e1926589bfa089438
dunnkers <jeroenoverschie@gmail.com>
2020-09-04 11:53:09 +0200

    [WIP] Running fs on Dask

--- dask/dasktest.py
+++ dask/dasktest.py
@@ -1,32 +1,54 @@
-from dask_jobqueue import SLURMCluster
-from dask import delayed
-from distributed import Client
+import pandas as pd
 
-print('initializing slurm cluster.')
-cluster = SLURMCluster(cores=2,
-                       processes=1,
-                       memory="4GB",
-                       project="woodshole2",
-                       walltime="00:05:00",
-                       queue="regular",
-                       interface="ib0")
-print('initialized.')
-cluster.scale(jobs=2)
-print(cluster.job_script())
-client = Client(cluster)
+print('initialized worker.')
 
-print('running client.compute')
-def step_1_w_single_GPU(data):
-    return "Step 1 done for: %s" % data
-def step_2_w_local_IO(data):
-    return "Step 2 done for: %s" % data
-stage_1 = [delayed(step_1_w_single_GPU)(i) for i in range(3)]
-stage_2 = [delayed(step_2_w_local_IO)(s2) for s2 in stage_1]
-result_stage_2 = client.compute(stage_2)
-print(result_stage_2)
-for res in result_stage_2:
-    print('result = ',res.result())
+from sklearn.feature_selection import chi2
+import numpy as np
 
-print(client.submit(lambda x: x + 1, 10).result())
+def chi2_ranking(X, y):
+    scores, _ = chi2(X, y)
+    ranking = np.argsort(-scores)
+    return ranking
 
-print('end of script.')
+if __name__ == '__main__':
+    from distributed import Client, LocalCluster
+    from dask import delayed
+    # from dask_jobqueue import SLURMCluster
+    # cluster = SLURMCluster(cores=2,
+    #                        processes=1,
+    #                        memory="4GB",
+    #                        project="woodshole2",
+    #                        walltime="00:05:00",
+    #                        queue="regular",
+    #                        interface="ib0")
+    # print(cluster.job_script())
+    client = Client()
+
+    data_collection = pd.read_csv('dask/descriptor.csv')
+    row = data_collection.iloc[0]
+    dataset = pd.read_csv(row.path, sep='\t')
+    X = dataset.drop('Class', axis=1).values
+    y = dataset['Class'].values
+    feature_names = dataset.drop('Class', axis=1).columns.values
+
+    from pymongo import MongoClient
+    import urllib.parse
+    username = urllib.parse.quote_plus('user')
+    password = urllib.parse.quote_plus('pass/word')
+    dbclient = MongoClient('mongodb://%s:VxOgKiNI16@127.0.0.1' % (username, password))
+
+    print('running client.compute')
+    def step_1_w_single_GPU(data):
+        return "Step 1 done for: %s" % data
+    def step_2_w_local_IO(data):
+        return "Step 2 done for: %s" % data
+    stage_1 = [delayed(step_1_w_single_GPU)(i) for i in range(3)]
+    stage_2 = [delayed(step_2_w_local_IO)(s2) for s2 in stage_1]
+    result_stage_2 = client.compute(stage_2)
+    print(result_stage_2)
+    for res in result_stage_2:
+        print('result = ',res.result())
+
+    print(client.submit(lambda x: x + 1, 10).result())
+
+    print('end of script.')
