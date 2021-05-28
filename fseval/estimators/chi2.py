75b4e59c53ec353b7ff9cccd3fb5e4a270df35d0
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-05-28 17:23:28 +0200

    Properly name chi2 class as Classifier

--- fseval/estimators/chi2.py
+++ fseval/estimators/chi2.py
@@ -2,7 +2,7 @@ from sklearn.base import BaseEstimator
 from sklearn.feature_selection import chi2
 
 
-class Chi2Regressor(BaseEstimator):
+class Chi2Classifier(BaseEstimator):
     def fit(self, X, y):
         scores, _ = chi2(X, y)
         self.feature_importances_ = scores
