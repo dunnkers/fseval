f5145affbe9d5a38befd8a4f949ab0f9fe110330
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-06-01 10:07:27 +0200

    Explicitly add matplotlib as dep.

--- setup.py
+++ setup.py
@@ -19,18 +19,22 @@ with open("README.md", "r") as fh:
             "hydra-core==1.1.0.rc1",
             "hydra-colorlog==1.1.0.dev1",
             "numpy==1.20.2",
-            "openml==0.12.1",
             "pandas==1.2.4",
             "scikit-learn==0.24.2",
+            "humanfriendly==9.1",
+            # callbacks / storage providers / adapter
             "wandb==0.10.31",
+            # adapters
+            "openml==0.12.1",
+            "l2x-synthetic==2.0.0",
+            # estimators
             "pytorch-tabnet==3.1.1",
             "skrebate==0.62",
-            "l2x-synthetic==2.0.0",
-            "humanfriendly==9.1",
             "xgboost==1.4.2",
             "Boruta==0.3",
             "featboost @ git+https://github.com/dunnkers/FeatBoost.git@support-cloning",
             "stability-selection @ git+https://github.com/dunnkers/stability-selection.git@master",
+            "matplotlib==3.4.2",  # required by stability-selection
         ],
         setup_requires=["black==21.4b2", "pytest-runner==5.3.0"],
         tests_require=["pytest==6.2.3"],
