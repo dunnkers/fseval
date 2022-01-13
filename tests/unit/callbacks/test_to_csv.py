f0e794196e89dd3a868e2aa49e9fe382a023d48a
Jeroen Overschie <jeroenoverschie@gmail.com>
2022-01-13 12:49:30 +0100

    Abstract away commons: callback testing

--- tests/unit/callbacks/test_to_csv.py
+++ tests/unit/callbacks/test_to_csv.py
@@ -1,99 +1,57 @@
 import tempfile
 from pathlib import Path
+from typing import Callable, cast
 
 import numpy as np
 import pandas as pd
 import pytest
 from fseval.callbacks.to_csv import CSVCallback
-from fseval.types import Task
+from fseval.types import Callback, Task
 from omegaconf import DictConfig, OmegaConf
+from overrides import overrides
+
+from ._common import BaseCallbackTest
 
 
-def test_init_no_params():
-    """Initialization should fail when no `dir` param was supplied."""
-    # no `dir`
-    with pytest.raises(AssertionError):
-        CSVCallback()
+class TestCSVCallback(BaseCallbackTest):
+    @overrides
+    def get_callback(self) -> Callback:
+        # Create temporary dir. Callback should support nested directories - and create
+        # them when they do not exist yet accordingly.
+        csv_dir = Path(tempfile.mkdtemp())
+        csv_dir = csv_dir / "some_sub_dir"
 
-    # should work
-    instance = CSVCallback(dir="some_dir")
-    assert isinstance(instance, CSVCallback)
+        # setup callback
+        callback = CSVCallback(dir=csv_dir)
 
+        return callback
 
-@pytest.fixture
-def config() -> DictConfig:
-    config: DictConfig = OmegaConf.create(
-        {
-            "dataset": {
-                "name": "some_dataset",
-                "n": 10000,
-                "p": 5,
-                "task": Task.classification,
-                "group": "some_group",
-                "domain": "some_domain",
-            },
-            "ranker": {
-                "name": "some_ranker",
-            },
-            "validator": {"name": "some_validator"},
-        }
-    )
+    @overrides
+    def restore_config(self, callback: Callback) -> pd.DataFrame:
+        callback = cast(CSVCallback, callback)
+        filepath = callback.dir / "experiments.csv"
+        df: pd.DataFrame = pd.read_csv(
+            filepath,
+            index_col="id",
+            parse_dates=["date_created"],
+        )
 
-    return config
+        return df
 
+    @overrides
+    def restore_table(self, callback: Callback, table_name: str) -> pd.DataFrame:
+        callback = cast(CSVCallback, callback)
+        filepath = callback.dir / f"{table_name}.csv"
+        df: pd.DataFrame = pd.read_csv(filepath, index_col="id")
 
-def test_on_begin(config: DictConfig):
-    """Test whether `on_begin` pipeline callback correctly stores experiment information
-    in a csv file called `experiments.csv`."""
+        return df
 
-    tmpdir = Path(tempfile.mkdtemp())
-    to_csv = CSVCallback(dir=tmpdir)
+    def test_init(self):
+        """Initialization should fail when no `dir` param was supplied."""
+        # no `dir`
+        with pytest.raises(AssertionError):
+            CSVCallback()
 
-    # pretend `starting` pipeline
-    to_csv.on_begin(config)
-
-    # retrieve table using Pandas
-    df: pd.DataFrame = pd.read_csv(
-        tmpdir / "experiments.csv", index_col="id", parse_dates=["date_created"]
-    )
-
-    # assert columns
-    assert "id" == df.index.name
-    assert "dataset" in df.columns
-    assert "dataset/n" in df.columns
-    assert "dataset/p" in df.columns
-
-    # assert types
-    assert np.isscalar(df["dataset/n"][0])
-    assert np.isscalar(df["dataset/p"][0])
-    assert "datetime" in str(df.dtypes["date_created"])
-
-
-def test_on_table(config: DictConfig):
-    """Tests whether the callback successfully stores data in csv files when calling the
-    `on_table` callback."""
-
-    # create temporary dir, with one nesting level. this ensures the callback is able
-    # to create subdirectories by itself.
-    tmpdir = Path(tempfile.mkdtemp()) / "some_sub_dir"
-    to_csv = CSVCallback(dir=tmpdir)
-
-    # store dataframe in table
-    df_to_store = pd.DataFrame([{"some_metric": 343}])
-    name = "some_table"
-    to_csv.on_begin(config)  # necessary to set `self.id`
-    to_csv.on_table(df_to_store, name)
-
-    # this table should be appended to the csv file, not replacing the file.
-    df_to_store = pd.DataFrame([{"some_metric": 400}])
-    to_csv.on_table(df_to_store, name)
-
-    # retrieve using Pandas
-    filepath: Path = tmpdir / "some_table.csv"
-    df: pd.DataFrame = pd.read_csv(filepath, index_col="id")
-
-    # assert table columns and record types
-    assert "id" == df.index.name
-    assert "some_metric" in df.columns
-    assert np.isscalar(df["some_metric"][0])
-    assert len(df) == 2
+        # assert instantiation in __init__ was successfull
+        callback = self.get_callback()
+        assert isinstance(callback, CSVCallback)
--- tests/unit/callbacks/test_to_sql.py
+++ tests/unit/callbacks/test_to_sql.py
@@ -1,87 +1,42 @@
+from typing import cast
+
 import numpy as np
 import pandas as pd
 import pytest
 from fseval.callbacks.to_sql import SQLCallback
-from fseval.types import Task
+from fseval.types import Callback, Task
 from omegaconf import DictConfig, OmegaConf
+from overrides import overrides
+
+from ._common import BaseCallbackTest
 
 
-def test_init_no_params():
-    """Initialization should fail when no `engine` param was supplied."""
-    # no `engine`
-    with pytest.raises(AssertionError):
-        SQLCallback()
+class TestSQLCallback(BaseCallbackTest):
+    @overrides
+    def get_callback(self) -> Callback:
+        self.callback = SQLCallback(engine={"url": "sqlite://"})  # in-memory database
+        return self.callback
 
-    # no `engine.url`
-    with pytest.raises(AssertionError):
-        SQLCallback(engine={})
+    @overrides
+    def restore_config(self, callback: Callback) -> pd.DataFrame:
+        callback = cast(SQLCallback, callback)
+        df: pd.DataFrame = pd.read_sql(
+            "experiments", con=callback.engine, index_col="id"
+        )
+        return df
 
+    @overrides
+    def restore_table(self, callback: Callback, table_name: str) -> pd.DataFrame:
+        callback = cast(SQLCallback, callback)
+        df: pd.DataFrame = pd.read_sql(table_name, con=callback.engine, index_col="id")
+        return df
 
-@pytest.fixture
-def config() -> DictConfig:
-    config: DictConfig = OmegaConf.create(
-        {
-            "dataset": {
-                "name": "some_dataset",
-                "n": 10000,
-                "p": 5,
-                "task": Task.classification,
-                "group": "some_group",
-                "domain": "some_domain",
-            },
-            "ranker": {
-                "name": "some_ranker",
-            },
-            "validator": {"name": "some_validator"},
-        }
-    )
+    def test_init(self):
+        """Initialization should fail when no `engine` param was supplied."""
+        # no `engine`
+        with pytest.raises(AssertionError):
+            SQLCallback()
 
-    return config
-
-
-@pytest.fixture
-def to_sql() -> SQLCallback:
-    to_sql = SQLCallback(engine={"url": "sqlite://"})
-    return to_sql
-
-
-def test_on_begin(to_sql: SQLCallback, config: DictConfig):
-    """Test whether `on_begin` pipeline callback correctly stores experiment information
-    in the `experiments` table."""
-
-    # pretend `starting` pipeline
-    to_sql.on_begin(config)
-
-    # retrieve table using Pandas
-    df: pd.DataFrame = pd.read_sql("experiments", con=to_sql.engine, index_col="id")
-
-    # assert columns
-    assert "id" == df.index.name
-    assert "dataset" in df.columns
-    assert "dataset/n" in df.columns
-    assert "dataset/p" in df.columns
-
-    # assert types
-    assert np.isscalar(df["dataset/n"][0])
-    assert np.isscalar(df["dataset/p"][0])
-    assert "datetime" in str(df.dtypes["date_created"])
-
-
-def test_on_table(to_sql: SQLCallback, config: DictConfig):
-    """Tests whether the callback successfully stores data in a table when calling the
-    `on_table` callback."""
-
-    # store dataframe in table
-    df_to_store = pd.DataFrame([{"some_metric": 343}])
-    name = "some_table"
-    to_sql.on_begin(config)  # necessary to set `self.id`
-    to_sql.on_table(df_to_store, name)
-
-    # retrieve using Pandas
-    df: pd.DataFrame = pd.read_sql("some_table", con=to_sql.engine, index_col="id")
-
-    # assert table columns and record types
-    assert "id" == df.index.name
-    assert "some_metric" in df.columns
-    assert np.isscalar(df["some_metric"][0])
-    assert len(df) == 1
+        # no `engine.url`
+        with pytest.raises(AssertionError):
+            SQLCallback(engine={})
