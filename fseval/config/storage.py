bda5c3e364f5fec0385ae02a6bdf2fa5f68d8900
Jeroen Overschie <jeroenoverschie@gmail.com>
2022-01-23 11:54:48 +0100

    Work on storage config

--- fseval/config/storage.py
+++ fseval/config/storage.py
@@ -6,6 +6,21 @@ from omegaconf import MISSING
 
 @dataclass
 class StorageConfig:
-    _target_: str = MISSING
+    """
+    Allows you to define a storage for loading and saving cached estimators, among other
+    files, like the hydra and fseval configuration in YAML.
+
+    Attributes:
+        load_dir (Optional[str]): Defines a path to load files from. Must point to
+            exactly the directory containing the files, i.e. you should not point to a
+            higher-level directory than where the files are. Path can be relative or
+            absolute, but an absolute path is recommended.
+        save_dir (Optional[str]): The directory to save files to. Can be relative or
+            absolute.
+    """
+
     load_dir: Optional[str] = None
     save_dir: Optional[str] = None
+
+    # required for instantiation
+    _target_: str = MISSING
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -85,10 +85,6 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
                 label: 'GitHub',
                 href: 'https://github.com/dunnkers/fseval',
               },
