2a6c4a96d7e9b27b6ce9347cd5ae1b43e4c5eed1
Jeroen Overschie <jeroenoverschie@gmail.com>
2022-01-25 21:56:23 +0100

    🔨 fix tests

--- examples/quick-start-structured-configs/conf/ranker/mutual_info.py
+++ examples/quick-start-structured-configs/conf/ranker/mutual_info.py
@@ -1,11 +1,11 @@
 from fseval.config import EstimatorConfig
 
 mutual_info_ranker = EstimatorConfig(
-  name= "Mutual Info",
-  estimator=dict(
-    _target_="benchmark.MutualInfoClassifier",
-  ),
-  _estimator_type="classifier",
-  multioutput=False,
-  estimates_feature_importances=True,
+    name="Mutual Info",
+    estimator=dict(
+        _target_="benchmark.MutualInfoClassifier",
+    ),
+    _estimator_type="classifier",
+    multioutput=False,
+    estimates_feature_importances=True,
 )
