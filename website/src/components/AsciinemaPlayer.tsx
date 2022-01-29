c495132e9aab006936e4f4d433bf0ab91e0b9e1c
Jeroen Overschie <jeroenoverschie@gmail.com>
2022-01-29 16:17:08 +0100

    Upgrade terminal example to use asciinema-player

---
+++ website/src/components/AsciinemaPlayer.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef } from 'react';
+import * as AsciinemaPlayerLibrary from 'asciinema-player';
+
+type AsciinemaPlayerProps = {
+    src: string;
+    // START asciinemaOptions
+    cols: string;
+    rows: string;
+    autoPlay: boolean
+    preload: boolean;
+    loop: boolean | number;
+    startAt: number | string;
+    speed: number;
+    idleTimeLimit: number;
+    theme: string;
+    poster: string;
+    fit: string;
+    fontSize: string;
+    // END asciinemaOptions
+};
+
+const AsciinemaPlayer: React.FC<AsciinemaPlayerProps> = ({
+    src,
+    ...asciinemaOptions
+}) => {
+    const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const currentRef = ref.current;
+        AsciinemaPlayerLibrary.create(src, currentRef, asciinemaOptions);
+    }, [src]);
+
+    return <div ref={ref} />;
+};
+
+export default AsciinemaPlayer;
